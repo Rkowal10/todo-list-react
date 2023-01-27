5832819d89604d1defa1bfb744a8acf4dd2ba2d2
Radoslaw Kowalczyk <rkowal64@gmail.com>
2023-01-27 16:38:30 +0100

    task list without interaction yet

---
+++ src/Buttons/index.js
@@ -0,0 +1,23 @@
+import "./style.css";
+
+const Buttons = (props) => {
+    if (props.tasks.length === 0) {
+        return null;
+    }
+
+    return (
+        <div className="buttons">
+            <button className="buttons__button">
+                {props.hideDoneTasks ? "Pokaż" : "Ukryj"} zakończone
+            </button>
+            <button className="buttons__button" disabled={props.tasks.every(({ done }) => done)}              
+            >Ukończ wszystkie
+            </button>
+            <button className="buttons__button"
+            >Usuń wszystkie
+            </button>
+        </div>
+    )
+};
+
+export default Buttons;
