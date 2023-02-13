161462c4d40db73fe4b4902f3de9f853b9ffe054
Radoslaw Kowalczyk <rkowal64@gmail.com>
2023-02-13 13:30:29 +0100

    function addNewTask

--- src/Form/index.js
+++ src/Form/index.js
@@ -1,16 +1,31 @@
+import { useState } from "react";
 import "./style.css";
 
-const Form = () => (
-  <form className="form">
-    <input 
-      className="form__input" 
-      placeholder="Co masz do zrobienia?" 
-      autoFocus={true} 
-    />
-    <button className="form__button"
-      >Dodaj zadanie
-    </button>
-  </form>
-);
+const Form = ({addNewTask}) => {
+  const [newTaskContent, setNewTaskContent] = useState("");
+
+  const onFormSubmit = (event) => {
+    event.preventDefault();
+    addNewTask(newTaskContent.trim());
+    setNewTaskContent("");
+  };
+
+
+  return (
+    <form className="form" onSubmit={onFormSubmit}>
+      <input
+        value={newTaskContent}
+        onChange={(event) => setNewTaskContent(event.target.value)}
+        className="form__input"
+        placeholder="Co masz do zrobienia?"
+        autoFocus={true}
+      />
+      <button className="form__button"
+      >
+        Dodaj zadanie
+      </button>
+    </form>
+  );
+};
 
 export default Form;
