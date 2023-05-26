3c5bbb2d14b517ddfb1b7793455b533e8153eb64
Radoslaw Kowalczyk <rkowal64@gmail.com>
2023-05-26 23:16:03 +0200

    added new colors and breakpoint

--- src/theme.js
+++ src/theme.js
@@ -1,3 +1,17 @@
 export const theme = {
-    
+    color: {
+        silver: "#bdbcbc",
+        white: "#fff",
+        japaneseLaurel: "#008000",
+        green: "#00b300",
+        red: "#ff0000",
+        sunsetOrange: "#ff4d4d",
+        teal: "#008080",
+        bondiBlue: "#00b3b3",
+        gray: "#808080",
+        black: "#000000",
+    },
+    breakpoint: {
+        mobile: 767,
+    },
 };
