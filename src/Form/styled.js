9dda637de951e9f0f4883c7f46c58881f12244bf
Radoslaw Kowalczyk <rkowal64@gmail.com>
2023-05-26 23:30:43 +0200

    changed name on StyledForm

--- src/Form/styled.js
+++ src/Form/styled.js
@@ -1,6 +1,6 @@
 import styled from "styled-components";
 
-export const Forms = styled.form`
+export const StyledForm = styled.form`
     display: grid;
     grid-template-columns: 1fr auto;
     gap: 20px;
