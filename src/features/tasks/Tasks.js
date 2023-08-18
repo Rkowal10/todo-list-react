69893e57cd18b04299d37c53e65d4f0a5ffc69fe
Radoslaw Kowalczyk <rkowal64@gmail.com>
2023-08-18 13:42:04 +0200

    removed unnecessary functions

--- src/features/tasks/Tasks.js
+++ src/features/tasks/Tasks.js
@@ -4,45 +4,19 @@ import TaskList from './TaskList';
 import Buttons from './Buttons';
 import Section from '../../common/Section';
 import Header from '../../common/Header';
-import { useTasks } from '../../useTasks';
-import { selectTasks } from './tasksSlice';
-import { useSelector } from 'react-redux';
 
 function Tasks() {
-
-  const { tasks } = useSelector(selectTasks);
-
-  const {
-    //tasks,
-    //hideDoneTasks,
-    //toggleHideDoneTasks,
-    removeTask,
-    //toggleTaskDone,
-    setAllTasksDone,
-    addNewTask,
-    removeAllTasks,
-  } = useTasks();
-
   return (
     <>
       <Header title="Lista zadań" />
       <Section
         title="Dodaj nowe zadanie"
-        body={<Form addNewTask={addNewTask} />}
+        body={<Form />}
       />
       <Section
         title="Lista zadań"
-        body={
-          <TaskList
-            removeTask={removeTask}
-          />
-        }
-        extraHeaderContent={
-          <Buttons
-            setAllTasksDone={setAllTasksDone}
-            removeAllTasks={removeAllTasks}
-          />
-        }
+        body={<TaskList />}
+        extraHeaderContent={<Buttons />}
       />
     </>
   );
