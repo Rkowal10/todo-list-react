4df58ce4de88b40d64f9b8fc1befd39764a5f153
Radoslaw Kowalczyk <rkowal64@gmail.com>
2023-09-13 12:53:41 +0200

    corrected incorrect semicolon

--- src/features/tasks/Buttons/index.js
+++ src/features/tasks/Buttons/index.js
@@ -24,7 +24,7 @@ const Buttons = () => {
                         Pobierz przykładowe zadania
                     </Button>
                 </>
-            )};
+            )}
             {!areTasksEmpty && (
                 <>
                     <Button
