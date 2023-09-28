07501ab802523a48a05fc3556290281cf2106411
Radoslaw Kowalczyk <rkowal64@gmail.com>
2023-09-28 12:12:17 +0200

    created new component TaskPage

---
+++ src/features/tasks/TaskPage/index.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import Section from '../../../common/Section';
+import Header from '../../../common/Header';
+import { useParams } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import { getTaskById } from '../tasksSlice';
+
+function TaskPage() {
+    const { id } = useParams();
+    const task = useSelector(state => getTaskById(state, id));
+
+    return (
+        <>
+            <Header title="Szczegóły zadania" />
+            <Section
+                title={task ? task.content : "Nie znaleziono zadania 🙁"}
+                body={<><strong>Ukończono:</strong> {task.done ? "Tak" : "Nie"}</>}
+            />
+        </>
+    );
+};
+
+export default TaskPage;
