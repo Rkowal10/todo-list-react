c2251e998a62f4d0bb7ae39a31286cf66e0a399b
Radoslaw Kowalczyk <rkowal64@gmail.com>
2023-10-02 11:11:28 +0200

    created new component Input

---
+++ src/features/tasks/Input/index.js
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export default styled.input`
+    padding: 10px;
+    border: 1px solid ${({ theme }) => theme.color.silver};
+
+    &:hover {
+        border: 1px solid ${({ theme }) => theme.color.black};
+    }
+`;
