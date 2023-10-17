36c0c559243a98bd03f7cb8d27c5a56c9901e7c6
Radoslaw Kowalczyk <rkowal64@gmail.com>
2023-10-17 11:23:39 +0200

    styled components List and Item

--- src/App.js
+++ src/App.js
@@ -1,33 +1,34 @@
-import { Link, Switch, Route, HashRouter, Redirect } from "react-router-dom/cjs/react-router-dom";
+import { Switch, Route, HashRouter, Redirect } from "react-router-dom/cjs/react-router-dom";
 import TasksPage from "./features/tasks/TasksPage";
 import TaskPage from "./features/tasks/TaskPage";
 import Author from "./features/author/Author";
+import { Item, List, StyledNavLink } from "./styled";
 
 export default () => (
     <HashRouter>
         <nav>
-            <ul>
-                <li>
-                    <Link to="/zadania">Zadania</Link>
-                </li>
-                <li>
-                    <Link to="/autor">O autorze</Link>
-                </li>
-            </ul>
-            <Switch>
-                <Route path="/zadania/:id">
-                    <TaskPage />
-                </Route>
-                <Route path="/zadania">
-                    <TasksPage />
-                </Route>
-                <Route path="/autor">
-                    <Author />
-                </Route>
-                <Route path="/">
-                    <Redirect to="/zadania" />
-                </Route>
-            </Switch>
+            <List>
+                <Item>
+                    <StyledNavLink to="/zadania">Zadania</StyledNavLink>
+                </Item>
+                <Item>
+                    <StyledNavLink to="/autor">O autorze</StyledNavLink>
+                </Item>
+            </List>
         </nav>
+        <Switch>
+            <Route path="/zadania/:id">
+                <TaskPage />
+            </Route>
+            <Route path="/zadania">
+                <TasksPage />
+            </Route>
+            <Route path="/autor">
+                <Author />
+            </Route>
+            <Route path="/">
+                <Redirect to="/zadania" />
+            </Route>
+        </Switch>
     </HashRouter>
 );
