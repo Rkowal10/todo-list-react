4dc097750cc94eab44a0dbbc18891c300820b955
Radoslaw Kowalczyk <rkowal64@gmail.com>
2023-10-17 13:35:05 +0200

    added error handling

--- src/features/tasks/TaskPage/index.js
+++ src/features/tasks/TaskPage/index.js
@@ -14,7 +14,11 @@ function TaskPage() {
             <Header title="Szczegóły zadania" />
             <Section
                 title={task ? task.content : "Nie znaleziono zadania 🙁"}
-                body={<><strong>Ukończono:</strong> {task.done ? "Tak" : "Nie"}</>}
+                body={!!task && (
+                    <>
+                        <strong>Ukończono:</strong> {task.done ? "Tak" : "Nie"}
+                    </>
+                )}
             />
         </>
     );
