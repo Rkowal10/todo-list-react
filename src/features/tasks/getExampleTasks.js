01f6838a11947954c0e628d7a0eeeec655b9f9dc
Radoslaw Kowalczyk <rkowal64@gmail.com>
2023-10-31 10:47:12 +0100

    added const requestUrl

--- src/features/tasks/getExampleTasks.js
+++ src/features/tasks/getExampleTasks.js
@@ -1,5 +1,7 @@
+const requestUrl = fetch("/todo-list-react/exampleTasks.json");
+
 export const getExampleTasks = async () => {
-    const response = await fetch("/todo-list-react/exampleTasks.json");
+    const response = await requestUrl;
     
     if (!response.ok) {
         new Error(response.statusText);
